a94738cea05f99fbfa93fb866aa9b6f16530a667
Aleksandr Golubev <aleksgolubev@prodgpu18748z23.h.o3.ru>
2021-06-25 16:47:47 +0300

    fix: utils modified

---
+++ utils.py
@@ -0,0 +1,46 @@
+import torch
+import numpy as np
+import os
+import random
+
+
+class AverageMeter(object):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def fetch_scheduler(optimizer):
+    if SCHEDULER =='ReduceLROnPlateau':
+        scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=factor, patience=patience, verbose=True, eps=eps)
+    elif SCHEDULER =='CosineAnnealingLR':
+        scheduler = CosineAnnealingLR(optimizer, T_max=T_max, eta_min=min_lr, last_epoch=-1)
+    elif SCHEDULER =='CosineAnnealingWarmRestarts':
+        scheduler = CosineAnnealingWarmRestarts(optimizer, T_0=T_0, T_mult=1, eta_min=min_lr, last_epoch=-1)
+    return scheduler
+
+
+def fetch_loss():
+    loss = nn.CrossEntropyLoss()
+    return loss
+
+
+def seed_torch(seed=42):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+
